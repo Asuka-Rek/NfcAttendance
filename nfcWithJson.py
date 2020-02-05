642e448b4bbb6c64de468508f3e160452231935f
Asuka-Rek <53948194+Asuka-Rek@users.noreply.github.com>
2020-02-05 18:57:32 +0900

    nfcWithJson.addNfc()についてGUI対応の準備とdocstringの追加

--- nfcWithJson.py
+++ nfcWithJson.py
@@ -25,21 +25,45 @@ def outputJson(authInfo):
 
 
 def addNfc(newCardHolder):
-    authInfo = importJson()
+    """
+    新しいカードと持ち主を登録するときに使う関数。
+    登録済みの名前もしくはカード、または非対応種カードのときは追加しない。
+    
+    Parameters
+    ----------
+    newCardHolder : str
+        カードの所有者として登録する名前
+    authInfo : dict
+        カード登録情報
+        {keys : values} = {'カードのidm()' : {カード所有者名}
 
+    Return
+    ----------
+    常にbool値と変数messageを返す。
+    bool値について
+        True  : カード情報の登録に成功したとき
+        False : カード情報が何らかの事情で登録できないとき
+    message : str
+        GUI画面に表示するためのメッセージを返す。
+        カード登録成功ならその旨、失敗ならその詳細。
+    """
+
+    # 現在のカード登録情報を読み込み
+    authInfo = importJson()
     if newCardHolder in authInfo.values():
-        print('その名前は登録済みです。別の名前で登録してください。')
-        return
+        message = 'その名前は登録済みです。別の名前で登録してください。'
+        return False, message
     
-    newCardSddRes = fe.inputCard()
-    if newCardSddRes in authInfo:
-        print('そのカードは既に登録されています。別のカードを試してください。')
-        return
-    if newCardSddRes == None:
-        print('対応していないカードです。別のカードを試してください。')
-        return
-    authInfo[newCardSddRes] = newCardHolder
-    
+    newCard_idm = fe.inputCard()
+    if newCard_idm in authInfo:
+        message = 'そのカードは既に登録されています。別のカードを試してください。'
+        return False, message
+    if newCard_idm == None:
+        message = '対応していないカードです。別のカードを試してください。'
+        return False, message
+
+    authInfo[newCard_idm] = newCardHolder
     outputJson(authInfo=authInfo)
-    print('登録が完了しました。')
+    message = '登録が完了しました。'
+    return True, message
 
